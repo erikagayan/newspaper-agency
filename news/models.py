442550ab3622cf1ad57c16f23a432dd6b9cd463e
Erik Agayan <erik.agayan1@gmail.com>
2023-07-20 17:05:09 +0300

    Order topics by id and add topic list in topic_list.html

--- news/models.py
+++ news/models.py
@@ -6,7 +6,7 @@ class Topic(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     class Meta:
-        ordering = ["name"]
+        ordering = ["id"]
 
     def __str__(self):
         return self.name
