dc12126539278598a69a015ea70bed0963fc4b13
Erik Agayan <erik.agayan1@gmail.com>
2023-07-20 13:35:47 +0300

    Add login view

--- news/views.py
+++ news/views.py
@@ -1,5 +1,27 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
+from news.models import Topic, Redactor, Newspaper
 
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+
+    num_newspapers = Newspaper.objects.count()
+    num_redactors = Redactor.objects.count()
+    num_topics = Topic.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_newspapers": num_newspapers,
+        "num_redactors": num_redactors,
+        "num_topics": num_topics,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "taxi/index.html", context=context)
